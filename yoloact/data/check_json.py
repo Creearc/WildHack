9c877888c494da4b75486919d1f461f44f93af49
Pavel <slivnitsin1997@mail.ru>
2021-12-04 22:35:32 +0500

    Update check_json.py

--- yoloact/data/check_json.py
+++ yoloact/data/check_json.py
@@ -1,6 +1,6 @@
 import json
 
-with open(r'D:/hackathon/WildHack/yolact/examples_data/cig_butts/waste/val/coco_annotations.json', 'r') as f:
+with open(r'/home/alexandr/wildhack/WildHack/yoloact/data/waste/val/coco_annotations.json', 'r') as f:
     json_data = json.load(f)
     
 print(json.dumps(json_data, indent=2))
