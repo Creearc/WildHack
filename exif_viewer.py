900b8eb98cb804a4621a6e176e6d5840389696b4
Alexandr <knxandr@rambler.ru>
2021-12-04 18:32:56 +0500

    Nikita
    @nlfamlv

--- exif_viewer.py
+++ exif_viewer.py
@@ -8,7 +8,7 @@ for f in os.listdir(path):
     image = Image(file)
     print(dir(image))
     print('Orientation: ', image.orientation)
-    print(image.gps_altitude)
+    print(image.subject_distance)
     print(f"Latitude: {image.gps_latitude} {image.gps_latitude_ref}")
     print(f"Longitude: {image.gps_longitude} {image.gps_longitude_ref}\n")
     print('_________________')
